bf5295bba0cb70b25d0032bb45e1afd813982488
Leejihyun <dlwlgus349@naver.com>
2017-10-31 13:45:30 +0900

    add insert BTDAO

--- FnW_Project/src/com/fnw/book/Book_TotalDAO.java
+++ FnW_Project/src/com/fnw/book/Book_TotalDAO.java
@@ -1,7 +1,33 @@
 package com.fnw.book;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.fnw.util.DBConnector;
+
 public class Book_TotalDAO {
-	public void insert() {}
+	public int insert(Book_TotalDTO book_TotalDTO) throws Exception
+	{
+		Connection con =  DBConnector.getConnect();
+		String sql = "insert into book_total values(?,?,?,?,?,?,?,?,?,?,?)";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setInt(1, book_TotalDTO.getNum());
+		st.setString(2, book_TotalDTO.getTitle());
+		st.setString(3, book_TotalDTO.getWriter());
+		st.setString(4, book_TotalDTO.getCompany());
+		st.setDate(5, book_TotalDTO.getPublish_date());
+		st.setString(6, book_TotalDTO.getSection());
+		st.setInt(7, book_TotalDTO.getLibrary());
+		st.setInt(8, book_TotalDTO.getType());
+		st.setInt(9, book_TotalDTO.getState());
+		st.setString(10, book_TotalDTO.getRent_id());
+		st.setInt(11, book_TotalDTO.getRent_count());
+		
+		int result = st.executeUpdate();
+		
+		DBConnector.disConnect(st, con);
+		return result;
+	}
 	public void delete() {}
 	public void update() {}
 	public void getTotalCount() {}
--- FnW_Project/src/com/fnw/book/Book_TotalDTO.java
+++ FnW_Project/src/com/fnw/book/Book_TotalDTO.java
@@ -1,6 +1,6 @@
 package com.fnw.book;
 
-import java.util.Date;
+import java.sql.Date;
 
 public class Book_TotalDTO {
 	private int num;
