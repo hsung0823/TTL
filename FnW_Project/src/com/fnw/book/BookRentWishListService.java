e9e8a3a344f270db7bf3b1f17c7be83cdae8adf0
HeeSeong <hsung0823@naver.com>
2017-11-02 14:51:54 +0900

    MyPage_paging 전부다 완료

--- FnW_Project/src/com/fnw/book/BookRentWishListService.java
+++ FnW_Project/src/com/fnw/book/BookRentWishListService.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.fnw.action.Action;
 import com.fnw.action.ActionFoward;
 /*import com.fnw.book.Book_Buy_WishDAO;*/
+import com.fnw.util.PageMaker;
 
 public class BookRentWishListService implements Action {
 
@@ -16,9 +17,33 @@ public class BookRentWishListService implements Action {
 		ActionFoward actionFoward = new ActionFoward();
 		Book_Rent_WishDAO book_Rent_WishDAO = new Book_Rent_WishDAO();
 		ArrayList<Book_Rent_WishDTO> list = new ArrayList<>();
+		
+		int curPage = 0;
 		try {
-			list = book_Rent_WishDAO.selectList(request.getParameter("id"));
+			curPage = Integer.parseInt(request.getParameter("curPage"));
+		} catch (Exception e) {
+			curPage = 1;
+		}
+		
+		String kind = request.getParameter("kind");
+		if(kind==null) {
+			kind="title";
+		}
+		String search = request.getParameter("search");
+		if(search==null) {
+			search="";
+		}
+		
+		String id = request.getParameter("id");
+		int totalCount = 0;
+		
+		try {
+			totalCount = book_Rent_WishDAO.getTotalCount(kind, search);
+			PageMaker pageMaker = new PageMaker(curPage, totalCount);
+			list = book_Rent_WishDAO.selectList(id,pageMaker.getMakeRow(),kind,search);
 			request.setAttribute("bookRentWishList", list);
+			request.setAttribute("id", id);
+			request.setAttribute("page", pageMaker.getMakePage());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
